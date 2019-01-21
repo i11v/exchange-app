8b078aea0964ec74d8d4469753a12a968baa0df0
Ilnur Khalilov <xomyaq@gmail.com>
2019-01-21 13:25:02 +0300

    Updates

--- precache-manifest.94aba8cb58700e01443dba27d092f534.js
+++ precache-manifest.94aba8cb58700e01443dba27d092f534.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    revision: "229c360febb4351a89df",
-    url: "/static/js/runtime~main.229c360f.js",
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    revision: "8bc82ad5db435ad49eb2",
-    url: "/static/js/main.8bc82ad5.chunk.js",
+    "revision": "8bc82ad5db435ad49eb2",
+    "url": "/static/js/main.8bc82ad5.chunk.js"
   },
   {
-    revision: "a7027e131a4a1b6a3eac",
-    url: "/static/js/1.a7027e13.chunk.js",
+    "revision": "a7027e131a4a1b6a3eac",
+    "url": "/static/js/1.a7027e13.chunk.js"
   },
   {
-    revision: "d7a25e47bc2502b9b2e9bf9b936cdb0d",
-    url: "/index.html",
-  },
+    "revision": "d7a25e47bc2502b9b2e9bf9b936cdb0d",
+    "url": "/index.html"
+  }
 ];
--- service-worker.js
+++ service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("/precache-manifest.94aba8cb58700e01443dba27d092f534.js");
+importScripts(
+  "/precache-manifest.94aba8cb58700e01443dba27d092f534.js"
+);
 
 workbox.clientsClaim();
 
@@ -29,5 +29,6 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
